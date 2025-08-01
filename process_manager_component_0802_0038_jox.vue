35ec02d53105058a99caceec1fce33c273f31750
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-01 17:38:57 +0100

    Create JS 进程管理器

---
+++ process_manager_component_0802_0038_jox.vue
@@ -0,0 +1,50 @@
+// 代码生成时间: 2025-08-02 00:38:56
+export default {
+  name: 'ProcessManager',
+  data() {
+    return {
+      processes: [],
+      nextId: 1
+    };
+  },
+  created() {
+    this.fetchProcesses();
+  },
+  methods: {
+    fetchProcesses() {
+      // 假设这是从后端获取进程列表的API调用
+      // 这里我们使用静态数据作为示例
+      this.processes = [
+        { id: this.nextId++, name: 'System Process' },
+        { id: this.nextId++, name: 'User Process' }
+      ];
+    },
+    addProcess() {
+      // 添加新的进程
+      this.processes.push({
+        id: this.nextId++,
+        name: `New Process ${this.nextId - 1}`
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.process-manager {
+  font-family: Arial, sans-serif;
+}
+.process-manager h2 {
+  color: #333;
+}
+.process-manager ul {
+  list-style: none;
+  padding: 0;
+}
+.process-manager li {
+  padding: 5px 0;
+}
+.process-manager button {
+  margin-top: 20px;
+}
+</style>
