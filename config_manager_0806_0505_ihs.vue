80a7cc2f554dddbb953dc470e76bfb731b971642
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-05 22:05:16 +0100

    Add 配置文件管理器

---
+++ config_manager_0806_0505_ihs.vue
@@ -0,0 +1,39 @@
+// 代码生成时间: 2025-08-06 05:05:15
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'ConfigManager',
+  props: {
+    initialConfig: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    // 响应式数据
+    const config = ref(props.initialConfig);
+    const saveMessage = ref('');
+
+    // 生命周期钩子
+    onMounted(() => {
+      console.log('组件已挂载');
+    });
+
+    // 方法
+    function saveConfig() {
+      console.log('保存配置:', config.value);
+      saveMessage.value = '配置已保存';
+    }
+
+    return {
+      config,
+      saveMessage,
+      saveConfig,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* 此处可添加样式 */
+</style>
