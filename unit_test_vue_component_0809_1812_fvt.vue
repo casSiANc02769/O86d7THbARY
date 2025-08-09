8ff213ef38af6ffad2a8c740fb46dc128520ea58
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-09 11:12:06 +0100

    Setup 单元测试框架 using VUE

---
+++ unit_test_vue_component_0809_1812_fvt.vue
@@ -0,0 +1,38 @@
+// 代码生成时间: 2025-08-09 18:12:04
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const message = ref('Hello, Unit Test!');
+const count = ref(0);
+
+// 定义响应式数据的操作函数
+function increment() {
+  count.value++;
+}
+
+// 适当的生命周期钩子
+onMounted(() => {
+  console.log('Component is mounted.');
+});
+
+// 导出响应式数据和操作函数供模板使用
+export {
+  message,
+  count,
+  increment
+};
+</script>
+
+<style scoped>
+/* 这里可以添加组件特有的样式 */
+</style>
+
+<script>
+// 单元测试
+test('increments the count', () => {
+  const { count, increment } = require('./unit_test_vue_component.vue').default;
+  expect(count.value).toBe(0);
+  increment();
+  expect(count.value).toBe(1);
+});
+</script>
