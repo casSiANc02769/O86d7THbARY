8ae7afb1f9457bfa9764592f5baaae3aef0de0a9
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-13 22:08:51 +0100

    Enhance JS 配置文件管理器

---
+++ config_manager_0814_0508_bou.vue
@@ -0,0 +1,42 @@
+// 代码生成时间: 2025-08-14 05:08:50
+import { defineComponent, ref, reactive, onMounted } from 'vue';
+
+export default defineComponent({
+  name: 'ConfigManager',
+  props: {},
+  setup() {
+    const configData = reactive({});
+    const configKey = ref('');
+    const configValue = ref('');
+
+    function addConfig() {
+      if (configKey.value && configValue.value) {
+        configData[configKey.value] = configValue.value;
+      }
+    }
+
+    function removeConfig(key) {
+      delete configData[key];
+    }
+
+    onMounted(() => {
+      console.log('Component mounted');
+    });
+
+    return {
+      configData,
+      configKey,
+      configValue,
+      addConfig,
+      removeConfig
+    };
+  }
+});
+</script>
+
+<style scoped>
+.config-manager {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
