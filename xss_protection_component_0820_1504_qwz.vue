33eff84a9253b41f47fe9d200a526cfb0e650e3d
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-20 08:04:29 +0100

    Update new XSS攻击防护 feature

---
+++ xss_protection_component_0820_1504_qwz.vue
@@ -0,0 +1,57 @@
+// 代码生成时间: 2025-08-20 15:04:27
+import DOMPurify from 'dompurify'; // DOMPurify is a library used to sanitize HTML from XSS
+
+export default {
+  name: 'XssProtectionComponent',
+  data() {
+    return {
+      userInput: '',
+      sanitizedInput: '',
+      showAlert: false
+    };
+# 改进用户体验
+  },
+  watch: {
+    userInput(newVal) {
+      if (this.containsXSS(newVal)) {
+# TODO: 优化性能
+        this.showAlert = true;
+      } else {
+        this.showAlert = false;
+        this.sanitizeInput();
+      }
+    }
+  },
+  mounted() {
+    this.sanitizeInput(); // Sanitize input on load to avoid XSS via initial content
+  },
+  methods: {
+    handleInput(event) {
+      // When user types, check for XSS and update alert status
+      if (this.containsXSS(event.target.value)) {
+# TODO: 优化性能
+        this.showAlert = true;
+      } else {
+        this.showAlert = false;
+      }
+    },
+    sanitizeInput() {
+      // Use DOMPurify to sanitize the input
+      this.sanitizedInput = DOMPurify.sanitize(this.userInput);
+    },
+    containsXSS(input) {
+      // Simple XSS detection (for demonstration purposes)
+      const xssRegex = /<script|<iframe|onload|onerror/i;
+      return xssRegex.test(input);
+# FIXME: 处理边界情况
+    }
+  }
+};
+</script>
+
+<style>
+# 添加错误处理
+.alert {
+  color: red;
+}
+</style>
