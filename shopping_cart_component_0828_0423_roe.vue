135a7726134790b484b9ffbba4ec46608e3bb10c
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-27 21:23:01 +0100

    Introduce 购物车功能实现 using VUE

---
+++ shopping_cart_component_0828_0423_roe.vue
@@ -0,0 +1,34 @@
+// 代码生成时间: 2025-08-28 04:23:00
+import { reactive, onMounted } from 'vue';
+
+// Reactivity state
+const cart = reactive({
+  items: [],
+  totalQuantity: 0,
+});
+
+const addItem = () => {
+  const newItem = {
+    id: cart.items.length + 1,
+    name: 'Sample Product',
+    quantity: 1,
+  };
+  cart.items.push(newItem);
+  cart.totalQuantity += newItem.quantity;
+};
+
+// Lifecycle Hook
+onMounted(() => {
+  console.log('Component is mounted and cart is initialized.');
+});
+
+export default {
+  name: 'ShoppingCart',
+  setup() {
+    return {
+      cart,
+      addItem,
+    };
+  },
+};
+</script>
