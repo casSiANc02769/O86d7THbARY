6478b0a3a70528bef4651ffb38ffc8aa2cbe24f7
casSiANc02769 <malls_strata.0i@icloud.com>
2025-08-31 20:39:49 +0100

    Update JS 数据模型设计

---
+++ reactive_data_component_0901_0339_nbh.vue
@@ -0,0 +1,26 @@
+// 代码生成时间: 2025-09-01 03:39:46
+import { ref, onMounted, onUnmounted } from 'vue';
+
+// 响应式数据
+const message = ref('Hello, Vue 3!');
+const counter = ref(0);
+
+// 方法
+function incrementCounter() {
+  counter.value += 1;
+}
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('Component is mounted.');
+});
+
+onUnmounted(() => {
+  console.log('Component is unmounted.');
+});
+
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
