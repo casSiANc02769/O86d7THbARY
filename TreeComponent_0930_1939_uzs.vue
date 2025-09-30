7b05022db4f0cd0aa1ef72be7cfd16344c9b4990
casSiANc02769 <malls_strata.0i@icloud.com>
2025-09-30 12:39:38 +0100

    Update 树形结构组件

---
+++ TreeComponent_0930_1939_uzs.vue
@@ -0,0 +1,49 @@
+// 代码生成时间: 2025-09-30 19:39:37
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const treeData = ref([
+  {
+    id: 1,
+    label: 'Node 1',
+    children: [
+      { id: 2, label: 'Child 1 of Node 1', children: [] },
+      { id: 3, label: 'Child 2 of Node 1', children: [] },
+    ],
+  },
+  {
+    id: 4,
+    label: 'Node 2',
+    children: [],
+  },
+]);
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('Component is mounted');
+  // 可以在这里进行数据获取等初始化操作
+});
+
+</script>
+
+<template>
+  <div class="tree-component">
+    <!-- 树形结构展示 -->
+    <ul>
+      <li v-for="node in treeData" :key="node.id">
+        {{ node.label }}
+        <ul v-if="node.children.length">
+          <li v-for="child in node.children" :key="child.id">
+            {{ child.label }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.tree-component ul {
+  list-style-type: none;
+}
+</style>
