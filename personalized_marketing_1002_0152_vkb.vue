345c93ca5bbff28b2f1bf0e6cc0cc124c9ab7379
casSiANc02769 <malls_strata.0i@icloud.com>
2025-10-01 18:52:23 +0100

    Develop 个性化营销

---
+++ personalized_marketing_1002_0152_vkb.vue
@@ -0,0 +1,63 @@
+// 代码生成时间: 2025-10-02 01:52:22
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const userPreferences = ref({
+  sports: false,
+  fashion: true,
+  technology: false,
+});
+
+// 计算属性，根据用户偏好生成个性化营销信息
+const marketingMessage = ref('');
+
+const updateMarketingMessage = () => {
+  switch (true) {
+    case userPreferences.value.sports:
+      marketingMessage.value = 'Check out the latest sports gear!';
+      break;
+    case userPreferences.value.fashion:
+      marketingMessage.value = 'Discover the latest fashion trends!';
+      break;
+    case userPreferences.value.technology:
+      marketingMessage.value = 'Stay updated with the latest tech gadgets!';
+      break;
+    default:
+      marketingMessage.value = 'Welcome to our store!';
+  }
+};
+
+// 监听用户偏好变化
+watch(userPreferences, updateMarketingMessage);
+
+// 生命周期钩子，在组件挂载后更新营销信息
+onMounted(() => {
+  updateMarketingMessage();
+});
+
+</script>
+
+<template>
+  <div>
+    <h1>Personalized Marketing</h1>
+    <!-- 显示个性化营销信息 -->
+    <p>{{ marketingMessage }}</p>
+
+    <!-- 用户偏好选择 -->
+    <div>
+      <label>
+        <input type="checkbox" v-model="userPreferences.sports"/> Sports
+      </label>
+      <label>
+        <input type="checkbox" v-model="userPreferences.fashion"/> Fashion
+      </label>
+      <label>
+        <input type="checkbox" v-model="userPreferences.technology"/> Technology
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* 可添加一些简单的CSS样式 */
+</style>
